043cae316ef35c21759c5107ad79100f8a510b76
Wes <wwe@CliBox>
2016-01-16 14:42:44 -0800

    first commit

---
+++ web/index.js
@@ -0,0 +1,73 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var net = require('net');
+
+// Do really dumb error handling (basically prevent crashes!)
+process.on('uncaughtException',function(err){
+    console.log('something terrible happened..')
+})
+
+// Serve html
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+
+io.on('connection', function(socket){
+    console.log('a user connected');   
+    // open new connection to tadpole server
+    var client = new net.Socket();
+    client.connect(13370, '127.0.0.1', function() {
+	console.log('Connected to game server');
+    });
+    // respond to commands from server
+    client.on('data', function(data) {
+	var chr = String.fromCharCode(data[0]); 
+	console.log("got some data:" + data)
+	if( chr === 'N' ){
+	    console.log("connection is legit, sending name");
+	    client.write('PLAYERNAME');
+	}
+	if( chr === 'X' ){
+	    console.log("there ain't no room, dropping");
+	    client.destroy();
+	}
+	if( chr === 'S' ){
+	    console.log("connection invalid, dropping");
+	    client.destroy();
+	}
+	if( chr === 'D' ){
+	    console.log("server is terminating us");
+	    client.destroy();
+	}
+	if( chr === 'K' ){
+	    console.log("YOU DIED!");
+	    client.destroy();
+	}
+    });
+    // clean up when server closes connection
+    client.on('close', function() {
+	client.destroy();
+	console.log('Connection closed');
+    });
+    
+    // transmit button presses to tadpole server
+    socket.on('action', function(msg){
+	console.log('action: ' + msg);
+	client.write(msg);
+    });
+    // chat room? why not.
+    socket.on('chat message', function(msg){
+	console.log('message: ' + msg);
+    });
+    socket.on('disconnect', function(){
+	client.destroy(); // kill socket
+	console.log('user disconnected');
+    });
+});
+
+http.listen(3000, function(){
+    console.log('listening on *:3000');
+});
+
